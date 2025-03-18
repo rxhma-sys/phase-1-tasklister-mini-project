f1de952775375d79eafee4cc1aa62488cc73f23f
+ <+>
2025-03-19 01:21:46 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const todoList = [];
+
+ 
+  const taskForm = document.querySelector("#create-task-form");
+  
+  taskForm.addEventListener("submit", (event) => {
+    
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    todoList.push(taskInput.value.trim());
+    taskForm.reset(); 
+    renderTodoList(todoList);
+  });
 });
+
+function renderTodoList(todos) {
+  const unorderedListElement = document.querySelector("#tasks");
+  unorderedListElement.innerHTML = "";
+
+  todos.forEach((todo) => {
+   
+    const li = document.createElement("li");
+    li.textContent = todo;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.addEventListener("click", () => {
+      const index = todos.indexOf(todo);
+      todos.splice(index, 1);
+
+      renderTodoList(todos);
+    });
+
+    li.appendChild(deleteButton);
+    unorderedListElement.appendChild(li);
+  });
+}
